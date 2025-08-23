499fc72548e5d4bed7d5a85c59578a4be35964da
ynhule96 <Lenhuy10011996@gmail.com>
2025-08-22 23:02:40 -0400

    add getSinglePlant base on ID function

--- routes/plants-routes.ts
+++ routes/plants-routes.ts
@@ -2,6 +2,8 @@ import express from "express";
 import * as plantsControllers from "../controllers/plants-controllers.js";
 
 const router = express.Router();
+//return all the plants
 router.route("/allplants").get(plantsControllers.getAllPlants);
-
+//return single plant base on id
+router.route("/allplants/:id").get(plantsControllers.getSinglePlant)
 export default router;
