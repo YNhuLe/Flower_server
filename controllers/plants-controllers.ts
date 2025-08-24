48b7497db06d530cc21bc07e58f4690da9928463
ynhule96 <Lenhuy10011996@gmail.com>
2025-08-24 00:51:35 -0400

    add route for plant base on category

--- controllers/plants-controllers.ts
+++ controllers/plants-controllers.ts
@@ -15,30 +15,41 @@ const getAllPlants = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-
 //get plants base on id
-const getSinglePlant = async (req: Request, res: Response): Promise<void> =>{
-  try{
-    const {id} = req.params;
-const plant: Plant | undefined = await knex<Plant>("plants").where({id: Number(id)}).first();
+const getSinglePlant = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const plant: Plant | undefined = await knex<Plant>("plants")
+      .where({ id: Number(id) })
+      .first();
 
-if( !plant){
-  res.status(404).send("Plant not found.");
-  return;
-}
-res.status(200).json(plant);
-  }catch(error: any){
+    if (!plant) {
+      res.status(404).send("Plant not found.");
+      return;
+    }
+    res.status(200).json(plant);
+  } catch (error: any) {
     res.status(400).send(`Error retrieving plants: ${error.message || error}`);
   }
-}
+};
 
 //get list of plants base on category
-const getListPlant = async( req:Request, res: Response) : Promise<void> =>{
-  try{
-    const {id}= req.params;
-const plantList : [Plant] | undefined = await knex<Plant>("plants").where({id :Number(id)}).first;    
-  }catch(error: any){
-res.status(400).send(`Error retrieving plants base on category: ${error.message || error}`)
+const getPlantList = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const plantList = await knex("plants")
+      .join("categories", "plants.category_id", "categories.id")
+      .select("plants.*", "categories.*")
+      .where("categories.id", Number(id));
+    if (!plantList || plantList.length === 0) {
+      res.status(404).send("No plants found with the given category!");
+      return;
+    }
+    res.status(200).json(plantList);
+
+  } catch (error: any) {
+
+res.status(400).send(`Error fetching plants base on category ${error.message || error}`)
   }
-}
-export { getAllPlants, getSinglePlant};
+};
+export { getAllPlants, getSinglePlant , getPlantList};
