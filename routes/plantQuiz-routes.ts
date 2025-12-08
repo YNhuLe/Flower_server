75d18c3a2515e5fb06111c83125d91edfb32ebd6
ynhule96 <Lenhuy10011996@gmail.com>
2025-12-07 21:27:54 -0700

    update the gemini model to gemini-3, add more filter for the plants before AI recommendations

--- routes/plantQuiz-routes.ts
+++ routes/plantQuiz-routes.ts
@@ -5,5 +5,5 @@ import * as quizControllers from "../controllers/plantQuiz-controllers.js";
 const router = express.Router();
 
 router.route("/quiz").get(quizControllers.getQuizQuestionOptions);
-router.route("/quiz/answers").post(quizControllers.postQuizPlantAnswer);
+router.route("/quiz/answers").post(quizControllers.postQuizPlantRecommendations);
 export default router;
