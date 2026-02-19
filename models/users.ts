70ce5b92198e5e6049c1a327cb12fc80e71a2a7d
ynhule96 <Lenhuy10011996@gmail.com>
2026-02-18 23:09:17 -0700

    add user model

---
+++ models/users.ts
@@ -0,0 +1,11 @@
+interface User {
+    id: number;
+    name: string;
+    email:string;
+    password: string; 
+    phone_number: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export type {User};
