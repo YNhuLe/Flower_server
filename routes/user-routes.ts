cf0a53918e73f03b323e5f865e973a776815769b
ynhule96 <Lenhuy10011996@gmail.com>
2026-03-13 23:25:38 -0600

    add user route for auth0

--- routes/user-routes.ts
+++ routes/user-routes.ts
@@ -5,5 +5,10 @@ const router = express.Router();
 
 //add a user to the users table
 router.route("/users").post(userControllers.addUser);
+router.route("/auth/google").post(userControllers.verifyAuth0Token, userControllers.createOrCreateLoginGoogleUser);
 
+//route for user profile ( read-only)
+router.route("/users/me").get(userControllers.verifyAuth0Token, userControllers.getUserProfile);
+//route for editing user profile
+router.route("")
 export default router;
