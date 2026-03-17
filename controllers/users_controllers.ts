3a08e701bc7417fb409081e5efd485e04b487e29
ynhule96 <Lenhuy10011996@gmail.com>
2026-03-16 23:26:03 -0600

    change the getUserProfile to accept data from auth0 instead of firebase

--- controllers/users_controllers.ts
+++ controllers/users_controllers.ts
@@ -3,13 +3,9 @@ import configuration from "../knexfile";
 import type { Request, Response } from "express";
 import type { User } from "../models/users";
 import validator from "validator";
-
-import admin from "firebase-admin";
 import serviceAccount from "../serviceAccountKey.json";
+import { auth } from "express-oauth2-jwt-bearer/dist/index.js";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
 const knex = initKnex(configuration);
 
 // Controller function to add a new user from Email/Passwword sign up
@@ -51,6 +47,7 @@ const addUser = async (req: Request, res: Response): Promise<void> => {
         res.status(400).json({
           message: "User with this email already exists",
         });
+        return;
       }
 
       if (existingUser.phone_number === phone_number) {
@@ -58,6 +55,7 @@ const addUser = async (req: Request, res: Response): Promise<void> => {
           .status(400)
           .json({ message: "User with this phone number already exists" });
       }
+      return;
     }
   } catch (error: any) {
     res
@@ -77,49 +75,46 @@ const addUser = async (req: Request, res: Response): Promise<void> => {
 };
 
 //Verify the token that being sent from the Firebase
-const verifyFirebaseToken = async (
-  req: Request,
-  res: Response,
-  next: Function,
-) => {
-  const header = req.headers.authorization;
+if (!process.env.AUTH0_AUDIENCE) {
+  throw new Error("MIssing AUTH0_AUDIENCE in environment variables.");
+}
 
-  if (!header || !header.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  const token = header.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Missing token" });
-  }
-
-  try {
-    const decoded = await admin.auth().verifyIdToken(token);
-    req.user = decoded as any;
-    next();
-  } catch (err: any) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-};
-
+const verifyAuth0Token = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
+  tokenSigningAlg: "RS256",
+});
 // Controller function to add a new user from Google sign up
 const createOrCreateLoginGoogleUser = async (req: Request, res: Response) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "User not authenticated" });
+  const auth0User = req.auth as any;
+  if (!auth0User) {
+    return res.status(401).json({ message: "User not authenticated." });
   }
-  const { name = "", email, uid } = req.user;
+
+  const email = auth0User["https://eververdant.com/email"];
+  const name = auth0User["https://eververdant.com/name"];
+  const auth0_id = auth0User.sub;
+
+
+  if( !email || !name || !auth0_id) {
+    return res.status(400).json({ message: "Required user information missing in token." });
+  }
   try {
     const existingUser = await knex("users").where("email", email).first();
     if (!existingUser) {
       const [newUser] = await knex("users")
         .insert({
-          uid,
           email,
-          name: name,
+          name,
+          auth0_id,
           // ,avatar_url: picture
-        })
+          //in case user already exists with the same email but different auth0_id, we will update the name and auth0_id to the new one, this is for the case when user sign up with email/password first then later sign up with google with the same email, we want to link the google account to the existing user
+        }).onConflict("email").merge({ name, auth0_id })
         .returning("*");
+
+        if( !newUser){
+          return res.status(500).json({ message: "Failed to create user." });
+        }
       return res.status(201).json(newUser);
     }
     return res.status(200).json(existingUser);
@@ -129,4 +124,35 @@ const createOrCreateLoginGoogleUser = async (req: Request, res: Response) => {
   }
 };
 
-export { addUser, verifyFirebaseToken, createOrCreateLoginGoogleUser };
+//get user
+const getUserProfile = async (req: Request, res: Response) => {
+  if (!req.auth) {
+    return res.status(401).json({ message: "User not authenticated" });
+  }
+  const decoded = req.auth as any;
+ 
+  const email = decoded["https://eververdant.com/email"] || decoded.email;
+  const name = decoded["https://eververdant.com/name"] || decoded.name;
+  const auth0_id = decoded.sub;
+  console.log("Decoded token in getUserProfile: ", email, name, decoded);
+  if (!email || !auth0_id) {
+    return res.status(400).json({ message: "Email or Auth0 ID not found in token" });
+  }
+try{
+  const user = await knex("users").where(email ? { email } : { auth0_id }).first();
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.json(user);
+}catch(error: any){
+  console.error("Error fetching user profile: ", error);
+  res.status(500).json({ message: "Database error", error: error });
+}
+
+};
+export {
+  addUser,
+  verifyAuth0Token,
+  createOrCreateLoginGoogleUser,
+  getUserProfile,
+};
