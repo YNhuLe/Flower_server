57a0fdfe24f88f80feadd2624f63ab00ba23b972
ynhule96 <Lenhuy10011996@gmail.com>
2026-03-27 23:58:44 -0600

    add session controllers

---
+++ controllers/session_controllers.ts
@@ -0,0 +1,54 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+import type { Request, Response } from "express";
+const knex = initKnex(configuration);
+//GET /api/session/:session_id for guest users
+const getSessionById = async (req: any, res: any) => {
+  try {
+    const { session_id } = req.params;
+
+    //look up the session in the DB by session_id, if not found return 404
+    const session = await knex("quiz_sessions")
+      .where({ id: session_id })
+      .first();
+
+    if (!session) {
+      res.status(404).json({ error: "Session not found!" });
+      return;
+    }
+
+    //fetch top 3 plants recommended from the plants table based on the session.top3_plant_ids array
+    const recommendations = await knex("plants")
+      .whereIn("id", session.top3_plant_ids)
+      .select("*");
+    //loop through each plant and fetch its sizes from the plant_sizes tables, combine plants and the sizes into the object return to frontend
+    const sizes = await Promise.all(
+      recommendations.map(async (plant: any) => {
+        const plantSizes = await knex("plants_sizes")
+          .where({ plant_id: plant.id })
+          .select("*");
+        return { ...plant, plantSizes };
+      }),
+    );
+
+    res.status(200).json(
+        {
+            session_id: session.id,
+            recommendations: sizes,
+        }
+    )
+  } catch (error: any) {
+
+    res.status(400).json({
+        error: `Error fetching session data: ${error.message}`,
+    })
+  }
+};
+
+//GET session_id for logged in users by user_id
+
+const getSessionByUserId = async ( req:any, res:any) =>{
+
+}
+
+export {getSessionById}
