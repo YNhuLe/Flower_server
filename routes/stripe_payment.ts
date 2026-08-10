665fe7de47c7e40775dbe0d8f8aed40216abf7bc
ynhule96 <Lenhuy10011996@gmail.com>
2026-08-09 21:22:30 -0600

    add stripe payment route

---
+++ routes/stripe_payment.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import Stripe from 'stripe';
+
+const router = express.Router();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '');
+
+
+router.post('/create-payment-intent', async (req, res) =>{
+    try{
+        const { amount} = req.body;
+         const paymentIntent = await stripe.paymentIntents.create({
+            amount, 
+             currency: "cad",
+             automatic_payment_methods:{
+                enabled: true},
+             });
+res.send({clientSecret: paymentIntent.client_secret});
+       
+    }catch(error: any){
+        res.status(500).json({error: error.message});
+    }
+})
+
+export default router;
